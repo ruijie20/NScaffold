5b26010c39b575b5252548b20cf86e80532a78e9
xiaoyvr <xiaoyvr@gmail.com>
2012-10-08 23:24:04 +0800

    try to pack

--- pack.ps1
+++ pack.ps1
@@ -1,6 +1,9 @@
 $dir = Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) "tmp\pkgs\"
-remove-item .\tmp\pkgs\*.*
-
-src\nuget\NuGet.exe pack .\src\install\NScaffold.nuspec -NoPackageAnalysis -o $dir
+if(test-path .\tmp\pkgs\){
+	remove-item .\tmp\pkgs\*.*
+} else {
+	mkdir .\tmp\pkgs\
+}
+.\tools\nuget\NuGet.exe pack .\src\ns-install\NScaffold.nuspec -NoPackageAnalysis -o $dir
 $url = $dir.Replace("\", "/")
 cinst NScaffold -source "file:///$url" -force
