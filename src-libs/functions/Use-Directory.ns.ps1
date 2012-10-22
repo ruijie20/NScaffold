518080f64147c29c131a5b058723235af4ad992d
xiaoyvr <xiaoyvr@gmail.com>
2012-10-22 14:34:19 +0800

    upgrade nudeploy

--- src-libs/functions/Use-Directory.ns.ps1
+++ src-libs/functions/Use-Directory.ns.ps1
@@ -2,7 +2,7 @@ Function Use-Directory ($dir, [ScriptBlock]$action){
 	if(-not (Test-Path $dir)){
         New-Item $dir -Type Directory | Out-Null
     }
-    
+    Push-Location
     Set-Location $dir
     try{
         & $action
