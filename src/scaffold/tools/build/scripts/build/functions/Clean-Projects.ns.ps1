747f0c70c4e1cc0e9d0a5fda3ef6ec60d75568d9
xiaoyvr <xiaoyvr@gmail.com>
2013-01-26 00:22:54 +0800

    fix

--- src/scaffold/tools/build/scripts/build/functions/Clean-Projects.ns.ps1
+++ src/scaffold/tools/build/scripts/build/functions/Clean-Projects.ns.ps1
@@ -3,5 +3,7 @@ Function Clean-Projects ($projectDirs) {
         ? { Test-Path $_ } | 
         Get-ChildItem -include bin,obj -Recurse | 
         ? { $_.attributes -eq "Directory" } 
-    $foldersToDelete| % { Remove-Item "$_\*" -Recurse -Force}
+    if ($foldersToDelete) {
+        $foldersToDelete| % { Remove-Item "$_\*" -Recurse -Force}    
+    }    
 }
