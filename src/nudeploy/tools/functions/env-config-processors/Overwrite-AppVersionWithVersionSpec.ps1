0b55fd1427210b20733e3420c993a3528c079b4c
yjia <yjia@thoughtworks.com>
2013-04-24 00:29:59 +0800

    [JY] NA assert packages in versionspec existing in repo before deployment

--- src/nudeploy/tools/functions/env-config-processors/Overwrite-AppVersionWithVersionSpec.ps1
+++ src/nudeploy/tools/functions/env-config-processors/Overwrite-AppVersionWithVersionSpec.ps1
@@ -1,8 +1,30 @@
 Function Overwrite-AppVersionWithVersionSpec($envConfig, $versionSpecPath){
     $versionSpec = Import-VersionSpec $versionSpecPath
+    Assert-VersionSpec $versionSpec $envConfig.nugetRepo
     Apply-VersionSpec $envConfig $versionSpec
     Write-Host "Overwritten AppVersionWithVersionSpec:"
     $versionSpec | Out-Host
+}
+
+Function Test-PackageExisted($package, $version, $nugetRepo){
+    Write-Host "$nuget list $package -source $nugetRepo"
+    $allVersions = & $nuget list $package -source $nugetRepo -AllVersions 
+    if($allVersions -match "^$package $version$"){
+        $true
+    }else{
+        $false
+    }
+}
+
+Function Assert-VersionSpec($versionSpec, $nugetRepo){
+    $nuget = "$PSScriptRoot\tools\nuget\nuget.exe"
+    $versionSpec.keys |%{
+        $package = $_
+        $version = $versionSpec[$_]
+        if(-not (Test-PackageExisted $package $version $nugetRepo)){
+            throw "Package[$package] with version[$version] not found in repository[$nugetRepo]"
+        }
+    }
 }
 
 Function Import-VersionSpec($versionSpecPath) {
