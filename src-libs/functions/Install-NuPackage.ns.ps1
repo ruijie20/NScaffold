9b8ce97d679ba03134a3cd7d7618b47ab6ebf1f3
yingrui <yingrui@thoughtworks.com>
2014-02-09 12:57:55 +0800

    make ut work

--- src-libs/functions/Install-NuPackage.ns.ps1
+++ src-libs/functions/Install-NuPackage.ns.ps1
@@ -18,37 +18,38 @@ Function Install-NuPackage($package, $workingDir, [string]$version = "", [script
     $packageInstalled = @(& $nuget list $package -allversions -source $workingDir)
 
     if($packageInstalled -contains "$package $version"){ 
-        write-host "Already installed [$package $version]" -f cyan
-        return "$workingDir\$package.$version"
-    }
-
-    $cmd = "$nuget install $package $versionSection $sourceSection -OutputDirectory $workingDir 2>&1"
-    Write-Host "Executing: $cmd"
-    $nuGetInstallOutput = Iex "$cmd"
-
-    if($LastExitCode -ne 0){
-        throw "$nuGetInstallOutput"
-    }    
-
-    if($version){
         $installedVersion = $version
+        $nuGetInstallOutput = "Already installed [$package $installedVersion]"
+        Write-Host $nuGetInstallOutput -f cyan
     } else {
-        $installedVersion = "$nuGetInstallOutput" -match "(?i)\'$package (?<version>.*?)\'" | % { $matches.version }  
-    }
+        $cmd = "$nuget install $package $versionSection $sourceSection -OutputDirectory $workingDir 2>&1"
+        Write-Host "Executing: $cmd"
+        $nuGetInstallOutput = Iex "$cmd"
 
-    if ($nuGetInstallOutput -match "Unable") {
-        throw "$nuGetInstallOutput"
-    }
+        if($LastExitCode -ne 0){
+            throw "$nuGetInstallOutput"
+        }    
 
-    if(-not $installedVersion){
-        throw "$nuGetInstallOutput"
+        if($version){
+            $installedVersion = $version
+        } else {
+            $installedVersion = "$nuGetInstallOutput" -match "(?i)\'$package (?<version>.*?)\'" | % { $matches.version }  
+        }
+
+        if ($nuGetInstallOutput -match "Unable") {
+            throw "$nuGetInstallOutput"
+        }
+
+        if(-not $installedVersion){
+            throw "$nuGetInstallOutput"
+        }        
     }
 
     $packageDir = "$workingDir\$package.$installedVersion"
     Write-Host "Package [$package] has been downloaded to [$packageDir]." -f cyan
     if(($nuGetInstallOutput -match "Successfully installed") -or ($nuGetInstallOutput -match "already installed")){
         if($postInstall){
-            &$postInstall $packageDir | Out-Null         
+            &$postInstall $packageDir | Out-Default
         }
     }
     $packageDir
